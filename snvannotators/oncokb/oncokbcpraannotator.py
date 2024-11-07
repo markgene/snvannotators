2b7ec2be364975effa293228d0f7a598ea8e5e87
Mark J Chen <mjchen.gene@gmail.com>
2024-11-07 10:18:49 -0500

    feat: add OncokbCpraAnnotator

---
+++ snvannotators/oncokb/oncokbcpraannotator.py
@@ -0,0 +1,36 @@
+"""Annotate Cpra with OncoKB."""
+
+from typing import Optional
+
+from pyoncokb.annotations.genomicchangeannotator import GenomicChangeAnnotator
+from pyoncokb.models.indicatorqueryresp import IndicatorQueryResp
+from pyoncokb.oncokbapi import OncokbApi
+from snvmodels.cpra import Cpra
+from snvmodels.cpra.formatters import CpraOncokbGenomicChangeFormatter
+
+
+class OncokbCpraAnnotator:
+    """Annotate Cpra with OncoKB."""
+
+    def __init__(self, cpra: Cpra, oncokb_api: OncokbApi):
+        self.cpra = cpra
+        self.oncokb_api = oncokb_api
+
+    def annotate(self, cpra: Cpra, ref_genome: str) -> Optional[IndicatorQueryResp]:
+        """Annotate.
+
+        Returns:
+            Optional[IndicatorQueryResp]: OncoKB API data model of the same name.
+        """
+        self.check_ref_genome(ref_genome)
+        genomic_change = CpraOncokbGenomicChangeFormatter().format(cpra=cpra)
+        annotator = GenomicChangeAnnotator(
+            self.oncokb_api, genomic_change=genomic_change, ref_genome=self.ref_genome
+        )
+        indicator_query_resp = annotator.annotate()
+        return indicator_query_resp
+
+    @staticmethod
+    def check_ref_genome(ref_genome):
+        """Check reference genome is valid."""
+        assert ref_genome in ["GRCh37", "GRCh38"]
