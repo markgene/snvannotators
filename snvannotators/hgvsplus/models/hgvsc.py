04930fab125972d8364f589683e4899833e32217
Mark J Chen <mjchen.gene@gmail.com>
2024-11-10 07:26:29 -0500

    feat: validate HgvsC and create HgvsC from HgvsT

--- snvannotators/hgvsplus/models/hgvsc.py
+++ snvannotators/hgvsplus/models/hgvsc.py
@@ -2,7 +2,11 @@
 
 import logging
 
+from hgvs.easy import validate
+from hgvs.exceptions import HGVSInvalidIntervalError
 from hgvs.sequencevariant import SequenceVariant
+
+from snvannotators.hgvsplus.models import HgvsT
 
 logger = logging.getLogger(__name__)
 
@@ -19,15 +23,27 @@ class HgvsC(SequenceVariant):
         "deletion-insertion": "delins",
     }
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, soft_validation: bool = True, *args, **kwargs):
+        """init.
+
+        :param soft_validation: only raise errors when they are not recognized,
+            defaults to True. If False, raise errors no matter they are recognized
+            or not.
+        :type soft_validation: bool, optional
+        """
         super().__init__(*args, **kwargs)
+        self.soft_validation = soft_validation
         assert self.type == "c"
+        self.is_valid()
 
     @classmethod
-    def from_sequence_variant_c(cls, sequence_variant_c: SequenceVariant):
+    def from_sequence_variant_c(
+        cls, sequence_variant_c: SequenceVariant, soft_validation: bool = True
+    ):
         assert isinstance(sequence_variant_c, SequenceVariant)
         assert sequence_variant_c.type == "c"
         return cls(
+            soft_validation=soft_validation,
             ac=sequence_variant_c.ac,
             type=sequence_variant_c.type,
             posedit=sequence_variant_c.posedit,
@@ -39,6 +55,35 @@ class HgvsC(SequenceVariant):
             ac=self.ac, type=self.type, posedit=self.posedit, gene=self.gene
         )
         return sequence_variant_c
+
+    @classmethod
+    def from_hgvs_t(cls, hgvs_t: HgvsT, soft_validation: bool = True):
+        if hgvs_t.type != "c":
+            raise ValueError(f"hgvs_t.type {hgvs_t} must be of type c")
+        return cls(
+            soft_validation=soft_validation,
+            ac=hgvs_t.ac,
+            type=hgvs_t.type,
+            posedit=hgvs_t.posedit,
+            gene=hgvs_t.gene,
+        )
+
+    def is_valid(self) -> bool:
+        sequence_variant_t = self.to_sequence_variant_t()
+        try:
+            is_valid = validate(sequence_variant_t)
+        except HGVSInvalidIntervalError as err:
+            if "coordinate is out of bounds" in str(err):
+                if self.soft_validation:
+                    logger.warning(
+                        "%s. The error is usually seen for promoter variant, e.g. c.-124"
+                    )
+                    is_valid = True
+                else:
+                    raise
+            else:
+                raise
+        return is_valid
 
     def is_substitution(self) -> bool:
         """Is substitution?"""
