282b71ccafabad89b7d370df6706dbf009f33c3a
Mark J Chen <mjchen.gene@gmail.com>
2025-04-03 11:14:25 -0400

    feat: test KnowledgebaseItem

--- tests/snvannotation/knowledgebase/test_knowledgebase.py
+++ tests/snvannotation/knowledgebase/test_knowledgebase.py
@@ -4,19 +4,23 @@ import unittest
 
 from snvannotators.snvannotation.knowledgebase import Knowledgebase
 
-# from snvannotators.snvannotation.knowledgebaseitem import KnowledgebaseItem
-
 
 class KnowledgebaseTestCase(unittest.TestCase):
 
-    def test_example_1(self):
+    def test_valid_example_1(self):
         knowledgebase = Knowledgebase(
             name="Example 1", knowledgebase_type="variant", revision="v0.1"
         )
         self.assertTrue(isinstance(knowledgebase, Knowledgebase))
+        self.assertTrue(knowledgebase.is_valid())
+        
 
     def test_invalid_example_1(self):
         knowledgebase = Knowledgebase(
             name="Example 1", knowledgebase_type="invalid type", revision="v0.1"
         )
+        self.assertTrue(isinstance(knowledgebase, Knowledgebase))
         self.assertFalse(knowledgebase.is_valid())
+        with self.assertRaises(Exception) as context:
+            knowledgebase.validate()
+        self.assertTrue("knowledgebase_type invalid type must be" in context)
