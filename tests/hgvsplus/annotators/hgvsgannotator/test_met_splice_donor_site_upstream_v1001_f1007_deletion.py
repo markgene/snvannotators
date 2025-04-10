52ee82adf478a542aaa607e969792736815311d0
Mark J Chen <mjchen.gene@gmail.com>
2025-04-10 11:07:08 -0400

    feat: update Test HgvsGAnnotator class with in-frame deletion affecting the splice sites flanking MET exon 14 result.

--- tests/hgvsplus/annotators/hgvsgannotator/test_met_splice_donor_site_upstream_v1001_f1007_deletion.py
+++ tests/hgvsplus/annotators/hgvsgannotator/test_met_splice_donor_site_upstream_v1001_f1007_deletion.py
@@ -18,7 +18,7 @@ class HgvsGAnnotatorMetSpliceDonorSiteUpstreamV1001F1007DelTestCase(
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        sequence_variant_g = parse("NC_000007.13:g.116412016delGTAGACTACCGAGCTACTTTT")
+        sequence_variant_g = parse("NC_000007.13:g.116412015_116412035del")
         hgvs_g = HgvsG.from_sequence_variant_g(sequence_variant_g=sequence_variant_g)
         alt_aln_method = "splign"
         tss_upstream_limit = 20000
@@ -29,8 +29,8 @@ class HgvsGAnnotatorMetSpliceDonorSiteUpstreamV1001F1007DelTestCase(
             tss_upstream_limit=tss_upstream_limit,
             uncertain=uncertain,
         ).annotate()
-        cls.hgvs_g_formatted = "NC_000017.10:g.7577527del"
-        cls.hgvs_g_normalized_formatted = "NC_000017.10:g.7577528del"
-        cls.tx_ac = "NM_000546.5"
-        cls.hgvs_t_formmated = "NM_000546.5(TP53):c.754del"
-        cls.hgvs_p_formatted = "NP_000537.3:p.Leu252SerfsTer93"
+        cls.hgvs_g_formatted = "NC_000007.13:g.116412015_116412035del"
+        cls.hgvs_g_normalized_formatted = "NC_000007.13:g.116412016_116412036del"
+        cls.tx_ac = "NM_000245.3"
+        cls.hgvs_t_formmated = "NM_000245.3(MET):c.3001_3021del"
+        cls.hgvs_p_formatted = "NP_000236.2:p.Val1001_Phe1007del"
