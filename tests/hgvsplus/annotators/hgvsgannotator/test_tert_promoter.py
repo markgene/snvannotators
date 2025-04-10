b596c9284b71c2b63723828a8f9ecc82f16621d4
Mark J Chen <mjchen.gene@gmail.com>
2025-04-10 10:41:06 -0400

    refact: use Test template for HgvsGAnnotator class

--- tests/hgvsplus/annotators/hgvsgannotator/test_tert_promoter.py
+++ tests/hgvsplus/annotators/hgvsgannotator/test_tert_promoter.py
@@ -4,12 +4,13 @@ import unittest
 
 from hgvs.easy import parse
 
-from snvannotators.hgvsplus.annotators.hgvsannotation import HgvsAnnotation
 from snvannotators.hgvsplus.annotators.hgvsgannotator import HgvsGAnnotator
-from snvannotators.hgvsplus.models import HgvsG, HgvsP, HgvsT
+from snvannotators.hgvsplus.models import HgvsG
+
+from .hgvsgannotatortesttemplate import HgvsGAnnotatorTestTemplate
 
 
-class HgvsGAnnotatorTertPromoterTestCase(unittest.TestCase):
+class HgvsGAnnotatorTertPromoterTestCase(HgvsGAnnotatorTestTemplate, unittest.TestCase):
     """Test HgvsGAnnotator class with TERT promoter."""
 
     @classmethod
@@ -26,39 +27,8 @@ class HgvsGAnnotatorTertPromoterTestCase(unittest.TestCase):
             tss_upstream_limit=tss_upstream_limit,
             uncertain=uncertain,
         ).annotate()
+        cls.hgvs_g_formatted = "NC_000005.9:g.1295228G>A"
+        cls.hgvs_g_normalized_formatted = "NC_000005.9:g.1295228G>A"
         cls.tx_ac = "NM_198253.2"
-
-    def test_hgvs_annotation(self):
-        self.assertTrue(isinstance(self.hgvs_annotation, HgvsAnnotation))
-
-    def test_hgvs_annotation_hgvs_g(self):
-        self.assertTrue(isinstance(self.hgvs_annotation.hgvs_g, HgvsG))
-        self.assertEqual(str(self.hgvs_annotation.hgvs_g), "NC_000005.9:g.1295228G>A")
-
-    def test_hgvs_annotation_hgvs_g_normalized(self):
-        self.assertTrue(isinstance(self.hgvs_annotation.hgvs_g_normalized, HgvsG))
-        self.assertEqual(
-            str(self.hgvs_annotation.hgvs_g_normalized), "NC_000005.9:g.1295228G>A"
-        )
-
-    def test_hgvs_annotation_hgvs_t(self):
-        for hgvs_tp_annotation in self.hgvs_annotation.hgvs_tp_annotations:
-            self.assertTrue(
-                hgvs_tp_annotation.hgvs_t is None
-                or isinstance(hgvs_tp_annotation.hgvs_t, HgvsT)
-            )
-            if hgvs_tp_annotation.tx_ac == self.tx_ac:
-                self.assertEqual(
-                    str(hgvs_tp_annotation.hgvs_t), "NM_198253.2(TERT):c.-124C>T"
-                )
-
-    def test_hgvs_annotation_hgvs_p(self):
-        for hgvs_tp_annotation in self.hgvs_annotation.hgvs_tp_annotations:
-            self.assertTrue(
-                hgvs_tp_annotation.hgvs_p is None
-                or isinstance(hgvs_tp_annotation.hgvs_p, HgvsP)
-            )
-            if hgvs_tp_annotation.tx_ac == self.tx_ac:
-                self.assertEqual(
-                    str(hgvs_tp_annotation.hgvs_p), "NP_937983.2(TERT):p.?"
-                )
+        cls.hgvs_t_formmated = "NM_198253.2(TERT):c.-124C>T"
+        cls.hgvs_p_formatted = "NP_937983.2(TERT):p.?"
--- tests/hgvsplus/annotators/hgvsgannotator/test_tp53c754del.py
+++ tests/hgvsplus/annotators/hgvsgannotator/test_tp53c754del.py
@@ -8,8 +8,10 @@ from snvannotators.hgvsplus.annotators.hgvsannotation import HgvsAnnotation
 from snvannotators.hgvsplus.annotators.hgvsgannotator import HgvsGAnnotator
 from snvannotators.hgvsplus.models import HgvsG, HgvsP, HgvsT
 
+from .hgvsgannotatortesttemplate import HgvsGAnnotatorTestTemplate
 
-class HgvsGAnnotatorTp53c754delTestCase(unittest.TestCase):
+
+class HgvsGAnnotatorTp53c754delTestCase(HgvsGAnnotatorTestTemplate, unittest.TestCase):
     """Test HgvsGAnnotator class with TP53 c.754del."""
 
     @classmethod
@@ -26,39 +28,8 @@ class HgvsGAnnotatorTp53c754delTestCase(unittest.TestCase):
             tss_upstream_limit=tss_upstream_limit,
             uncertain=uncertain,
         ).annotate()
+        cls.hgvs_g_formatted = "NC_000017.10:g.7577527del"
+        cls.hgvs_g_normalized_formatted = "NC_000017.10:g.7577528del"
         cls.tx_ac = "NM_000546.5"
-
-    def test_hgvs_annotation(self):
-        self.assertTrue(isinstance(self.hgvs_annotation, HgvsAnnotation))
-
-    def test_hgvs_annotation_hgvs_g(self):
-        self.assertTrue(isinstance(self.hgvs_annotation.hgvs_g, HgvsG))
-        self.assertEqual(str(self.hgvs_annotation.hgvs_g), "NC_000017.10:g.7577527del")
-
-    def test_hgvs_annotation_hgvs_g_normalized(self):
-        self.assertTrue(isinstance(self.hgvs_annotation.hgvs_g_normalized, HgvsG))
-        self.assertEqual(
-            str(self.hgvs_annotation.hgvs_g_normalized), "NC_000017.10:g.7577528del"
-        )
-
-    def test_hgvs_annotation_hgvs_t(self):
-        for hgvs_tp_annotation in self.hgvs_annotation.hgvs_tp_annotations:
-            self.assertTrue(
-                hgvs_tp_annotation.hgvs_t is None
-                or isinstance(hgvs_tp_annotation.hgvs_t, HgvsT)
-            )
-            if hgvs_tp_annotation.tx_ac == self.tx_ac:
-                self.assertEqual(
-                    str(hgvs_tp_annotation.hgvs_t), "NM_000546.5(TP53):c.754del"
-                )
-
-    def test_hgvs_annotation_hgvs_p(self):
-        for hgvs_tp_annotation in self.hgvs_annotation.hgvs_tp_annotations:
-            self.assertTrue(
-                hgvs_tp_annotation.hgvs_p is None
-                or isinstance(hgvs_tp_annotation.hgvs_p, HgvsP)
-            )
-            if hgvs_tp_annotation.tx_ac == self.tx_ac:
-                self.assertEqual(
-                    str(hgvs_tp_annotation.hgvs_p), "NP_000537.3:p.Leu252SerfsTer93"
-                )
+        cls.hgvs_t_formmated = "NM_000546.5(TP53):c.754del"
+        cls.hgvs_p_formatted = "NP_000537.3:p.Leu252SerfsTer93"
