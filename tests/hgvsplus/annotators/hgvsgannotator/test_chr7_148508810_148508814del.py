6383c8aaa7ac9723b087665cd41cbc8f0fdb4af0
Mark J Chen <mjchen.gene@gmail.com>
2026-01-09 13:00:21 -0500

    feat: add test case for HgvsGAnnotator with NC_000007.13:g.148508810_148508814del variant

---
+++ tests/hgvsplus/annotators/hgvsgannotator/test_chr7_148508810_148508814del.py
@@ -0,0 +1,34 @@
+"""Test HgvsGAnnotator class with NC_000007.13:g.148508810_148508814del."""
+
+import unittest
+
+from hgvs.easy import parse
+
+from snvannotators.hgvsplus.annotators.hgvsgannotator import HgvsGAnnotator
+from snvannotators.hgvsplus.models import HgvsG
+
+from .hgvsgannotatortesttemplate import HgvsGAnnotatorTestTemplate
+
+
+class HgvsGAnnotatorChr7_148508809_GATGCT_G_TestCase(HgvsGAnnotatorTestTemplate, unittest.TestCase):
+    """Test HgvsGAnnotator class with NC_000007.13:g.148508810_148508814del."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        sequence_variant_g = parse("NC_000007.13:g.148508810_148508814del")
+        hgvs_g = HgvsG.from_sequence_variant_g(sequence_variant_g=sequence_variant_g)
+        alt_aln_method = "splign"
+        tss_upstream_limit = 20000
+        uncertain = False
+        cls.hgvs_annotation = HgvsGAnnotator(
+            hgvs_g=hgvs_g,
+            alt_aln_method=alt_aln_method,
+            tss_upstream_limit=tss_upstream_limit,
+            uncertain=uncertain,
+        ).annotate()
+        cls.hgvs_g_formatted = "NC_000007.13:g.148508810_148508814del"
+        cls.hgvs_g_normalized_formatted = "NC_000007.13:g.148508810_148508814del"
+        cls.tx_ac = "NM_198253.2"
+        cls.hgvs_t_formmated = "NM_198253.2(TERT):c.-124C>T"
+        cls.hgvs_p_formatted = "NP_937983.2(TERT):p.?"
